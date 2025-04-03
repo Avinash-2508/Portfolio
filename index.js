069afa44e92536d92dea333b5227a616109cfbda
Avinash-2508 <avinash.ponneboina@gmail.com>
2025-04-03 19:39:33 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#element', {
+    strings: ["Web Developer", "Cyber Security Enthusiast","Student."],
+    typeSpeed: 100,
+    loop: true
+});
+
+function sendMail() {
+    window.location.href = "mailto:avinash.ponneboina@gmail.com?subject=Inquiry&body=Hello,Thanks for coming Here You are are one step away. Let's connect!";
+}
