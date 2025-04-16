37e7a915a39c7f9f0e2413d6a9558a38f3189db2
Avinash <avinash.ponneboina@gmail.com>
2025-04-16 23:32:41 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var typed = new Typed('#element', {
-    strings: ["Web Developer", "Cyber Security Enthusiast","Student."],
-    typeSpeed: 100,
-    loop: true
-});
-
-function sendMail() {
-    window.location.href = "mailto:avinash.ponneboina@gmail.com?subject=Inquiry&body=Hello,Thanks for coming Here You are are one step away. Let's connect!";
-}
